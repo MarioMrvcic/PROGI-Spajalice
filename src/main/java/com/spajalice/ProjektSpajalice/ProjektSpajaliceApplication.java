ae833b3eff166e78042912511a7d4327ebc85526
Mario Mrvcic <mario.mrvcic1@gmail.com>
2023-10-27 00:00:10 +0200

    Napravljen Spring Boot projekt

---
+++ src/main/java/com/spajalice/ProjektSpajalice/ProjektSpajaliceApplication.java
@@ -0,0 +1,21 @@
+package com.spajalice.ProjektSpajalice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjektSpajaliceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektSpajaliceApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Hello World!";
+	}
+
+}
---
+++ src/test/java/com/spajalice/ProjektSpajalice/ProjektSpajaliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spajalice.ProjektSpajalice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektSpajaliceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
