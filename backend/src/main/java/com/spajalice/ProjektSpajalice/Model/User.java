0852b8619bbff27937b5bbb5b054aefc5c4f228b
Mario <mario.mrvcic1@gmail.com>
2023-11-17 17:39:01 +0100

    Promjena strukture.

--- backend/src/main/java/com/spajalice/ProjektSpajalice/Model/User.java
+++ backend/src/main/java/com/spajalice/ProjektSpajalice/Model/User.java
@@ -8,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.util.SimpleIdGenerator;
 
 import java.util.Collection;
 import java.util.List;
