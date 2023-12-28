35723cc8665b1459be101578c77ea89953219190
Tia Radošević <luka28rad@gmail.com>
2023-12-28 19:46:04 +0100

    dodan basic create event page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Login from './components/Login'
 import Register from './components/Register'
+import ManageEvent from './components/ManageEvent'
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/" element={<Main />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/manage_event" element={<ManageEvent />} />
             </Routes>
         </div>
     )
---
+++ frontend/src/components/ManageEvent.js
@@ -0,0 +1,71 @@
+import { useNavigate } from 'react-router-dom'
+import './ManageEvent.css'
+import { useState } from 'react';
+
+function ManageEvent(){
+    const navigate = useNavigate();
+    const [eventName, setEventName] = useState('');
+    const [eventType, setEventType] = useState('');
+    const [eventDate, setEventDate] = useState('');
+    const [eventStartTime, setEventStartTime] = useState('');
+    const [eventDuration, setEventDuration] = useState('');
+    const [eventDescription, setEventDescription] = useState('');
+
+    return (
+        <div className="manageEvent">
+            <h1>Manage Event:</h1>
+            <form className="manageEvent--form">
+                <label htmlFor="eventName">Event name:</label>
+                    <input
+                        type="text"
+                        id="eventName"
+                        name="eventName"
+                        value={eventName}
+                        onChange={(e) => setEventName(e.target.value)}
+                    />
+                    <label htmlFor="eventType">Event type:</label>
+                    <input
+                        type="text"
+                        id="eventType"
+                        name="eventType"
+                        value={eventType}
+                        onChange={(e) => setEventType(e.target.value)}
+                    />
+                    <label htmlFor="eventDate">Event date:</label>
+                    <input
+                        type="date"
+                        id="eventDate"
+                        name="eventDate"
+                        value={eventDate}
+                        onChange={(e) => setEventDate(e.target.value)}
+                    />
+                    <label htmlFor="eventStartTime">Event start time:</label>
+                    <input
+                        type="time"
+                        id="eventStartTime"
+                        name="eventStartTime"
+                        value={eventStartTime}
+                        onChange={(e) => setEventStartTime(e.target.value)}
+                    />
+                    <label htmlFor="eventDuration">Event duration:</label>
+                    <input
+                        type="time"
+                        id="eventDuration"
+                        name="eventDuration"
+                        value={eventDuration}
+                        onChange={(e) => setEventDuration(e.target.value)}
+                    />
+                    <label htmlFor="eventDescription">Event description:</label>
+                    <textarea
+                        type="text"
+                        id="eventDescription"
+                        name="eventDescription"
+                        value={eventDescription}
+                        onChange={(e) => setEventDescription(e.target.value)}
+                    />
+            </form>
+        </div>
+    )
+}
+
+export default ManageEvent;
