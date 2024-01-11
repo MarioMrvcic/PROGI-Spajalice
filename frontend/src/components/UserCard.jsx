0bf9f197be759e3d2fb69f6ea7645757bf50b2bf
Toma-test <tomazulj10@gmail.com>
2024-01-11 16:33:39 +0100

    Dodan view gumb na adminov pregled korisnika koji vodi na privatan profil korisnika

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -3,14 +3,23 @@ import "./UserCard.css";
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useNavigate } from "react-router-dom";
 
 const UserCard = (props) => {
+    const navigate = useNavigate();
+
+    const goToProfile = () => {
+        const encodedEmail = encodeURIComponent(props.email);
+        navigate(`/profile/${encodedEmail}`);
+    }
+
     return (
         <div className='userCard'>
             <p>{props.name}</p>
             <p>{props.email}</p>
             <p>{props.role}</p>
             <div className="actions">
+                <Button variant="outlined" onClick={goToProfile}>View</Button>
                 <Button variant="outlined">Edit</Button>
                 <IconButton aria-label="delete" onClick={() => props.deleteUser(props.email)}>
                     <DeleteIcon />
